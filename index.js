c0206b72ba4c2f5344a84f6e6529cf218ac7bec5
Simon Gaeremynck <gaeremyncks@gmail.com>
2013-03-25 11:07:50 +0000

    Removed old authenticated/authorized code. Relying on express middleware to set session cookie.

--- index.js
+++ index.js
@@ -14,158 +14,42 @@
  */
 
 var Crypto = require('crypto');
-var util = require('util');
 
 var settings = require('ep_etherpad-lite/node/utils/Settings.js');
-var authorManager = require("ep_etherpad-lite/node/db/AuthorManager");
 
-var staticRegex = /^\/(static|javascripts|pluginfw|api|locales.json|favicon.ico)/;
-
-
-///////////
-// Hooks //
-///////////
-
-
-/**
- * Checks if a user is authorized to view the pad.
- * @see http://etherpad.org/doc/v1.2.6/#index_authorize
- *
- * @param  {String}     hook            The hook name
- * @param  {Object}     args            The arguments passed into this hook.
- * @param  {Request}    args.req        The expressJS request object.
- * @param  {Response}   args.res        The expressJS response object.
- * @param  {Function}   args.next       The expressJS next method in the middleware
- * @param  {String}     args.resource   The path.
- * @param  {Function}   callback        Standard callback method.
- */
-exports.authorize = function(hook, args, callback) {
-    // Don't bother with authorizing static requests.
-    if (args.resource.substr(0, 3) !== '/p/') {
-        return callback([ true ]);
-    }
-
-    // Strip out the /p/ from /p/padname
-    if (args.req.session && args.req.session.user && args.req.session.user.username) {
-        var pad = args.req._parsedUrl.pathname.substr(3).replace(/:/g, '_');
-        if (args.req.session.pad[pad]) {
-            callback([ true ]);
-        } else {
-            callback([ false ]);
+exports.expressCreateServer = function(hook, args, callback) {
+    args.app.use(function(req, res, next) {
+        if (req.query.sessionID && verifySignature(req.query.tenantAlias, req.query.expires, req.query.sessionID, req.query.signature)) {
+            req.cookies.sessionID = req.query.sessionID;
+            res.cookie('sessionID', req.query.sessionID);
         }
-    } else {
-        callback([ false ]);
-    }
+        next();
+    });
+    callback();
 };
 
 /**
- * A hook for authenticating users.
- * @see http://etherpad.org/doc/v1.2.6/#index_authenticate
+ * Verifies a signature.
  *
- * @param  {String}     hook            The hook name.
- * @param  {Object}     args            The arguments passed into this hook.
- * @param  {Request}    args.req        The expressJS request object.
- * @param  {Response}   args.res        The expressJS response object.
- * @param  {Function}   args.next       The expressJS next method in the middleware
- * @param  {Function}   callback        Standard callback method.
+ * @param  {String}     tenantAlias     The alias of the tenant for which we're verifying a message.
+ * @param  {Number}     expires         When this signature expires.
+ * @param  {String}     body            The string on which the signature is generated.
+ * @param  {String}     signature       The signature for the body.
+ * @return {Boolean}                    Whether or not the passed in signature is correct.
  */
-exports.authenticate = function(hook, args, callback) {
-    if (_verifySignature(args.req.query.signature, args.req.query.tenantAlias, args.req.query.expires, args.req.query.contentId, args.req.query.principalId, args.req.query.username)) {
-        var username = args.req.query.username;
-        args.req.session.user = { 'is_admin': false, 'username': args.req.query.username };
-        args.req.session.pad = args.req.session.pad || {};
-        args.req.session.pad[args.req.query.contentId.replace(/:/g, '_')] = true;
-        callback([ true ]);
-    } else {
-        callback([ false ]);
-    }
-};
+var verifySignature = function(tenantAlias, expires, body, signature) {
+    expires = parseInt(expires, 10);
 
-/**
- * A hook for handling messages.
- * @see  http://etherpad.org/doc/v1.2.6/#index_handlemessage
- *
- * @param  {String}     hook            The hook name.
- * @param  {Object}     args            The arguments passed into this hook.
- * @param  {Object}     args.message    The incoming message.
- * @param  {Function}   callback        Standard callback method.
- */
-exports.handleMessage = function(hook, args, callback) {
-    if (args.message.type === 'CLIENT_READY' && args.message.token) {
-        // Set the authorname to the username that sits in the session.
-        // This came in earlier when doing the authentication.
-        var client_id = args.client.id;
-        var username = 'Unknown user ' + Math.round(Math.random()*10000);
-        if (args.client.manager.handshaken[client_id].session && args.client.manager.handshaken[client_id].session.user && args.client.manager.handshaken[client_id].session.user.username) {
-            username = args.client.manager.handshaken[client_id].session.user.username;
-        }
-        setAuthor(args.message.token, username, function(err) {
-            if (err) {
-                // Drop the message if something goes wrong.
-                return callback([ null ]);
-            }
-
-            // Otherwise the message can be passed on.
-            callback([ args.message ]);
-        });
-    } else if (args.message.type == "COLLABROOM" && args.message.data.type == "USERINFO_UPDATE") {
-        // Discard any user updates.
-        return callback([null]);
-    } else {
-        // Pass on any other messages.
-        return callback([args.message]);
-    }
-};
-
-
-/////////////////////
-// Utility methods //
-/////////////////////
-
-
-/**
- * Verifies a signature against some values.
- *
- * @param  {String}     signature   The signature to check.
- * @param  {String}     tenantAlias The tenant alias.
- * @param  {Number}     expires     When the signature expires (in ms since epoch.)
- * @param  {String}     contentId   The ID of the piece of OAE content that represents this pad.
- * @param  {String}     principalId The ID of the user who wants to access this pad.
- * @param  {String}     username    The name of the user who wants to access this pad.
- * @return {Boolean}                A boolean that is true when the signature matches, false otherwise.
- * @api private
- */
-var _verifySignature = function(signature, tenantAlias, expires, contentId, principalId, username) {
-    // Check if all variables are passed in.
-    if (!signature || !tenantAlias || !contentId || !principalId || !username) {
+    // Check the expiry date.
+    if (Date.now() > expires) {
         return false;
     }
 
-    // The signature should be defined in the main settings.json file.
-    var signKey = settings.ep_oae.signKey;
-
     // Check the signature.
-    var msg = tenantAlias + '#' + expires + '#' + principalId + ':' + contentId + ':' + username;
+    var msg = tenantAlias + '#' + expires + '#' + body;
+    var signKey = settings.ep_oae.signKey;
     var hmac = Crypto.createHmac('sha1', signKey);
     hmac.update(msg);
-    var sig = hmac.digest('hex');
-    return sig === signature;
+    var generatedSig = hmac.digest('hex');
+    return generatedSig === signature;
 };
-
-/**
- * Sets the authorname for a certain token.
- *
- * @param {String} token        The token that identifies a user.
- * @param {String} desiredName  The desired authorname.
- */
-var setAuthor = function (token, desiredName, callback) {
-    authorManager.getAuthor4Token(token, function(err, author) {
-        if (err || !author) {
-            console.error('Could not get an author for token %s: err', token, err);
-            return callback(err || new Error('No author returned for token ' + token));
-        }
-
-        authorManager.setAuthorName(author, desiredName);
-    });
-};
-
